bc743f8e20466f0fa068c4962450502713f2e4b6
Vasilii Ostrovskii <Nezero@mail.ru>
2023-04-17 00:34:35 +0900

    main++

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[]houses=new int[20];
+        for (int i = 0; i < houses.length; i++) {
+            houses[i]=i+1;
+        }
+        for (int i = 0; i < houses.length; i++) {
+            if(houses[i]%2 !=0);{
+                houses[i]+=1;
+
+            }
+
+        }
+        for (int i=2 ;i<7; i++)
+        System.out.println(houses[i]);
     }
 }
